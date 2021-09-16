a7452545561a29bd52b0fb321832658880d23a53
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-09-16 12:05:45 -0300

    Update admin.py

--- doeagora/admin.py
+++ doeagora/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Doador, Instituicao, Produto, Doacao, Beneficiario
 
-# Register your models here.
+admin.site.register(Doador)
+admin.site.register(Instituicao)
+admin.site.register(Produto)
+admin.site.register(Doacao)
+admin.site.register(Beneficiario)
+
