c33875fbf53e2dae20a2a41eff49458eaa7800ac
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-09-17 10:16:58 -0300

    Update models.py

--- doeagora/models.py
+++ doeagora/models.py
@@ -6,6 +6,10 @@ class Doador(models.Model):
   endereco = models.CharField(max_length=100, verbose_name="endereço" )
   cpf = models.CharField(max_length=11, verbose_name="CPF")
   
+  class Meta:
+	  verbose_name = "Doador"
+	  verbose_name_plural = "Doadores"
+    
   def __str__(self):
     return self.nome
   
@@ -14,6 +18,10 @@ class Instituicao(models.Model):
   nome = models.CharField(max_length=40)
   data_ultima_doacao = models.DateField(verbose_name="data da última doação")
   
+  class Meta:
+	  verbose_name = "Instituição"
+	  verbose_name_plural = "Instituições"
+    
   def __str__(self):
     return self.nome
   
@@ -21,6 +29,10 @@ class Instituicao(models.Model):
 class Produto(models.Model):
   descricao = models.CharField(max_length=40, verbose_name="descrição")
   
+  class Meta:
+	  verbose_name = "Produto"
+	  verbose_name_plural = "Produtos"
+    
   def __str__(self):
     return self.descricao
   
@@ -32,6 +44,10 @@ class Beneficiario(models.Model):
   cpf = models.CharField(max_length=11, verbose_name="CPF")
   instituicao = models.ForeignKey(Instituicao, verbose_name="instituição", on_delete=models.PROTECT)
   
+  class Meta:
+	  verbose_name = "Beneficiário"
+	  verbose_name_plural = "Beneficiários"
+    
   def __str__(self):
     return self.nome
  
@@ -42,4 +58,8 @@ class Doacao(models.Model):
   doador = models.ForeignKey(Doador, on_delete=models.PROTECT)
   produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
   instituicao = models.ForeignKey(Instituicao, on_delete=models.PROTECT)
+  
+  class Meta:
+	  verbose_name = "Doação"
+	  verbose_name_plural = "Doações"
   
