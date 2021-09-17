aca3691cd1e5293cce5c1d1028851b6d1df3f936
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-09-17 10:27:12 -0300

    Update views.py

--- doeagora/views.py
+++ doeagora/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views import generic
+from . import models
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'doegora/index.html'
