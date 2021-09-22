fdb33158176a83c366b2225d0bfc3c47474f19d1
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-09-22 08:13:42 -0300

    Update urls.py

--- doeagora/urls.py
+++ doeagora/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('sosdoeinformacoes', views.SosDoeInfoView.as_view(), name='sosdoeinfo'),  
     path('cadastro-beneficiarios', views.CadastroBeneficiariosView.as_view(), name='c.beneficiario'),  
     path('cadastro-doadores', views.CadastroDoadoresView.as_view(), name='c.doadores'),
-    path('cadastro-instituicao', views.CadastroInstituicaoView.as_view(), name='c.instituicao'),
+    path('cadastro-instituição', views.CadastroInstituicaoView.as_view(), name='c.instituição'),
     path('fim-para-beneficiados', views.FimParaBeneficiadosView.as_view(), name='fim p beneficiados'),
     path('ver-beneficiario/<int:pk>', views.VerBeneficiarioView.as_view(), name='ver_beneficiario'),
-    path('ver-instituicao/<int:pk>', views.VerInstituicaoView.as_view(), name='ver_instituicao'),
-    path('ver-doador/<int:pk>', views.VerDoadorView.as_view(), name='ver_doador'),
+    path('ver-instituição/<int:pk>', views.VerInstituicaoView.as_view(), name='ver_instituição'),
+    path('ver-doadores/<int:pk>', views.VerDoadorView.as_view(), name='ver_doadores'),
 ]
