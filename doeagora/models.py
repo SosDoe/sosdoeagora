01bc35db12ba55675d39c28d6278c70203d4a36e
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-09-23 14:34:10 -0300

    ok

--- doeagora/models.py
+++ doeagora/models.py
@@ -69,5 +69,5 @@ class Doacao(models.Model):
 class Login(models.Model):
   email = models.CharField(max_length=40)
 
-    def __str__(self):
-    return self.nome
+  def __str__(self):
+  return self.nome
