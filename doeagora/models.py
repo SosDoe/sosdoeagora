1316cb0600e3a91cb4ba411f22fe909dfecf2e5e
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-10-06 14:13:45 -0300

    Update models.py

--- doeagora/models.py
+++ doeagora/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+user.last_name = 'Lennon'
+user.save()
+
 class Doador(models.Model): 
     nome = models.CharField(max_length=40)
     telefone = models.CharField(max_length=11)
