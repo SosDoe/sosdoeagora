3999cea8a545c69ed985c2c4f258712cb8174b81
SosDoe <90460041+SosDoe@users.noreply.github.com>
2021-10-19 15:23:26 -0300

    ok

--- doeagora/views.py
+++ doeagora/views.py
@@ -92,7 +92,7 @@ class VerDoacaoBeneficiarioView(generic.DetailView):
     context_name = 'doacao'
 
 class VerListagemDoacaoView(generic.ListView):
-    template_name = 'doeagora/verdoacao.html'
+    template_name = 'doeagora/listagemdoacao.html'
     model = models.Doacao
     context_object_name = 'doacoes'
 
